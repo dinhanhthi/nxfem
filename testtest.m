9303b852e7e73893ad143785c492e33ed94f25ac
Anh-Thi DINH <dinhanhthi@gmail.com>
2018-10-19 14:10:27 +0200

    ud phi

--- testtest.m
+++ testtest.m
@@ -1,3 +1,2 @@
-system('/users/home/blouza/MshDist/build/mshdist mshdist/phi')
-
-% system('mshdist mshdist/phi');
+% system('/users/home/blouza/MshDist/build/mshdist mshdist/phi')
+system('mshdist mshdist/phi');
