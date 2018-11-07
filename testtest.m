37c1744127d3473576b1a82d00242f49569ee4f3
Anh-Thi DINH <dinhanhthi@gmail.com>
2018-11-07 16:58:33 +0100

    thi: test tren ghost xem popup close duoc khong

--- testtest.m
+++ testtest.m
@@ -1,23 +1,24 @@
-% pdemesh(points,edges,triangles); % plot mesh
-%     hold on;
-    
-    % plot intersections
-        nCTs = size(iPs,3);
-        for it=1:nCTs
-            plot(iPs(1,:,it),iPs(2,:,it),'-r','LineWidth',1);
-            hold on
-        end
-        
-        % plot segments Gh conciding to mesh's edges
-        segment = getMeshSegmentOnGh(msh,pa,phi);
-        for it=1:size(segment,2)
-            plot(points(1,segment(:,it)),points(2,segment(:,it)),'-r','LineWidth',1);
-            hold on
-        end
-       
-    
-    gvx = gvnew.x; gvy = gvnew.y;
-    gvx(1,NCTs1(5,:))=0; gvy(1,NCTs1(5,:))=0;
-    gvx(1,NCTs2(5,:))=0; gvy(1,NCTs2(5,:))=0;
-    pdeplot(points,edges,triangles(1:3,:),'FlowData',[gvx; gvy]); 
-    hold off;
+x=[1,2,3];
+y=[1,5,1];
+y2=[-3,-1,0];
+
+f=figure; 
+set(f, 'Visible', 'off');
+plot(x,y,'-r');
+fileName = 'test.png';
+print(fileName,'-dpng','-r0');
+hold on
+
+plot(x,y2,'-b');
+fileName = 'test.png';
+print(fileName,'-dpng','-r0');
+
+g=figure; 
+set(g, 'Visible', 'off');
+plot(x,y,'-r');
+fileNameg = 'testg.png';
+print(fileNameg,'-dpng','-r0');
+close(g);
+
+close(f);
+
