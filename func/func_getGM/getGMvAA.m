32e1111a603bfb3d8eae1a7525f233f847c1635d
Anh-Thi DINH <dinhanhthi@gmail.com>
2018-11-12 05:38:44 +0100

    linh tinh

--- func/func_getGM/getGMvAA.m
+++ func/func_getGM/getGMvAA.m
@@ -10,6 +10,28 @@ function Av = getGMvAA(tris,phi,vold,wS,CT,msh,pa,cpV,cpW)
 %	 	 - nodesOmg2GamCTs : to get new nodes in CTs2
 % Output: global stiffness matrix Av
 
+
+    function L = getLv(cpV)
+        % Get L in int_Gam L*phi*phi
+        % L in this case: lam*{}{} + lam*kap1*kap2*[][]
+        % Related file: getGMvAA.m, getTriplePPoG.m
+        % Input:
+        % Output: matrix L: 4 x nCTs
+        %           row 1: Aij
+        %           row 2: Akikj
+        %           row 3: Akij
+        %           row 4: Aikj
+        kap1 = cpV.kap1; kap2 = cpV.kap2;
+        lambda = cpV.lambda;
+        nCTs = size(lambda,2);
+        L = zeros(4,nCTs);
+        L(1,:) = lambda.*kap1;
+        L(2,:) = lambda.*kap2;
+        % L(3,:) = 0 from zeros
+        % L(4,:) = 0 from zeros
+    end
+
+
 CTs=tris.CTs; NCTs1=tris.NCTs1; NCTs2=tris.NCTs2; 
 aChild=CT.areaChild; iPs=CT.iPs; uN=CT.uN;
 newNodes = msh.newNodes; % convert i to k(i)
