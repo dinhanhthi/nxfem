fb619f82c5200c0005111b7e96a1b689724ee9f4
Anh-Thi Dinh <dinhanhthi@gmail.com>
2018-11-29 15:09:28 +0000

    google test 17b+18 running

--- main_chopp06combine.m
+++ main_chopp06combine.m
@@ -71,9 +71,9 @@ model = model_chopp06combine;    % choose model. cf. file model_chopp2007.m
 
 %% NEED TO BE CHANGED EVERY TEST CASE
 savePlot = 1; % wanna save plot or not?
-    testCase = '17a'; % count the test and used to name the folder
+    testCase = '17b'; % count the test and used to name the folder
     pathOption = 'findGood';
-    moreInfo = 'TesT 17a: find best for chopp06 ([R]restart). After changing to FreeNodes. Giong 17 but bcu3=1e-4'; % write inside file txt
+    moreInfo = 'TesT 17b: find best for chopp06 ([R]restart). After changing to FreeNodes. Giong 17 but bcu3=5e-4+GP1e-5'; % write inside file txt
 
 %%
 showPlot = 0; % wanna show plots?
@@ -94,9 +94,9 @@ useNewton = 1; % use Newton to solve nonlinear problems?
     itol = 1e-4;
     
 % ghost penalty
-pa.useGP = 0; % wanna use ghost penalty term?
-    pa.gam1 = 1e-7; % parameter for 1st term
-    pa.gam2 = 1e-7 ; % parameter for 2nd term
+pa.useGP = 1; % wanna use ghost penalty term?
+    pa.gam1 = 1e-5; % parameter for 1st term
+    pa.gam2 = 1e-5 ; % parameter for 2nd term
 
 % Fast marching method
 useFMM = 0; % use fast marching method or not (mshdist)?
@@ -158,7 +158,7 @@ end
 pa.muS1 = 8.54932; pa.muS2 = 0;
 pa.muP1 = 8.28785; pa.muP2 = 0;
 %pa.bcu3 = 8.3e-6; % boundary condition for u on \pt\Omg_3
-pa.bcu3 = 1e-4; % testing
+pa.bcu3 = 5e-4; % testing
 cpU.kk1 = 146.88; cpU.kk2 = 183.6; % diff coef for u
 cpV.kk1 = 1; cpV.kk2 = 1;    % diff coef for v
 pa.f = 0.5; % volume fraction of active biomass
